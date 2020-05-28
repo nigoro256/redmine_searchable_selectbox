ae99a25d8af9e73095e00f952b08c8b89991d36c
nigoro256 <16351024+nigoro256@users.noreply.github.com>
2020-05-28 12:52:04 +0900

    Modify description

--- init.rb
+++ init.rb
@@ -3,6 +3,8 @@ require 'searchable_selectbox/hook_listener'
 
 Redmine::Plugin.register :redmine_searchable_selectbox do
   name 'Redmine Searchable Selectbox'
-  description "This plugin changes Redmine's selectbox searchable."
+  description "This plugin changes Redmine's selectbox searchable. (Customized https://github.com/ishikawa999/redmine_searchable_selectbox)"
+  url 'https://github.com/nigoro256/redmine_searchable_selectbox'
+  author 'nigoro256'
   version '1.0.0'
 end
